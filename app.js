49d52e3ae35c94ef2e311cead835385a7225cbd7
Aditya Sharma <53703526+itsjohnty@users.noreply.github.com>
2023-10-29 22:48:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const {
 const fs = require("fs");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const accountName = "tattelstorage";
 const accountKey =
